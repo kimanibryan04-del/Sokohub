5358f4eac843972cba20a2c5ee5ef97ea78cc3d1
kimanibryan04-del <kimanibryan04@gmail.com>
2026-03-30 13:23:29 +0300

    initial commit

---
+++ app/src/main/java/com/kim/sokohub/ui/screens/payment/PaymentScreen.kt
@@ -0,0 +1,23 @@
+package com.kim.sokohub.ui.screens.payment
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PaymentScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun PaymentScreenPreview(){
+    PaymentScreen()
+}
