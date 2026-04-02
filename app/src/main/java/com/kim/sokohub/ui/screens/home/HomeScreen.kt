60c102e4f5a291bf7bb03d20d650c761b3436186
kimanibryan04-del <kimanibryan04@gmail.com>
2026-04-02 13:45:29 +0300

    initial commit

--- app/src/main/java/com/kim/sokohub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/kim/sokohub/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,25 @@
 package com.kim.sokohub.ui.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,9 +33,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kim.sokohub.R
 import com.kim.sokohub.ui.theme.newme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,15 +56,15 @@ fun HomeScreen(){
             title = { Text(text = "Home") },
             navigationIcon = {
                 IconButton(onClick = {}) {
-                    Icon(imageVector = Icons.Default.Menu, contentDescription = "menu")
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "")
                 }
             },
             actions = {
                 IconButton(onClick = {}) {
-                    Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Menu")
+                    Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "")
                 }
                 IconButton(onClick = {}) {
-                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "Menu")
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "")
                 }
 
             },
@@ -57,22 +75,146 @@ fun HomeScreen(){
         )
         //End of topbar
 
-        Spacer(
-            modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         // Start of Searchbar
         var search by remember { mutableStateOf("") }
 
         TextField(
             value = search,
-            onValueChange = { search =it}
+            onValueChange = { search =it},
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+            leadingIcon = {Icon(imageVector = Icons.Default.Search, contentDescription = "Search")},
+            placeholder = {Text(text = "Search products, Category..." )}
+        )
+        //End of searchbar
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Featured Products",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 20.dp)
         )
 
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //row
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.grocery),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+                )
+
+                Text(
+                    text = "Groceries",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(newme),
+                    shape = RoundedCornerShape(size = 20.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                ) {
+                    Text(
+                        text = "Shop Now"
+                    )
+
+                }
+
+
+
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.g),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+                )
+
+                Text(
+                    text = "Groceries",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(newme),
+                    shape = RoundedCornerShape(size = 20.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                ) {
+                    Text(
+                        text = "Shop Now"
+                    )
+
+                }
 
 
 
 
-        //End of searchbar
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.gr),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+                )
+
+                Text(
+                    text = "Groceries",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(newme),
+                    shape = RoundedCornerShape(size = 20.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                ) {
+                    Text(
+                        text = "Shop Now"
+                    )
+
+                }
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+        //end of row
 
 
 
