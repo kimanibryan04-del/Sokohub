334294c191aa48f36df52099705ac03a04a96677
kimanibryan04-del <kimanibryan04@gmail.com>
2026-04-22 13:14:36 +0300

    new commit

--- app/src/main/java/com/kim/sokohub/navigation/AppNavHost.kt
+++ app/src/main/java/com/kim/sokohub/navigation/AppNavHost.kt
@@ -3,9 +3,11 @@ package com.kim.sokohub.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.kim.sokohub.navigation.ROUT_REGISTER
 import com.kim.sokohub.ui.screens.about.AboutScreen
 import com.kim.sokohub.ui.screens.auth.LoginScreen
@@ -14,6 +16,9 @@ import com.kim.sokohub.ui.screens.home.HomeScreen
 import com.kim.sokohub.ui.screens.intent.IntentScreen
 import com.kim.sokohub.ui.screens.onboarding.OnBoardingScreen
 import com.kim.sokohub.ui.screens.payment.PaymentScreen
+import com.kim.sokohub.ui.screens.products.AddProductScreen
+import com.kim.sokohub.ui.screens.products.UpdateProductScreen
+import com.kim.sokohub.ui.screens.products.ViewProductScreen
 import com.kim.sokohub.ui.screens.scalfold.ScaffoldScreen
 import com.kim.sokohub.ui.screens.service.ServiceScreen
 import com.kim.sokohub.ui.screens.splash.SplashScreen
@@ -61,6 +66,19 @@ fun AppNavHost(
             ScaffoldScreen(navController)
         }
 
+        composable(ROUTE_ADD_PRODUCT) { AddProductScreen(navController) }
+
+        composable(ROUTE_VIEW_PRODUCTS) { ViewProductScreen(navController) }
+
+        composable(
+            ROUTE_UPDATE_PRODUCT,
+            arguments = listOf(navArgument("productId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val productId = backStackEntry.arguments?.getString("productId")!!
+            UpdateProductScreen(navController, productId)
+        }
+
 
     }
+
 }
